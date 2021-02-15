6c8960fd200d2c68e99710ccc1ee42c644da9226
Gerard <gerardbonetbustos@gmail.com>
2021-02-15 15:01:46 +0100

    Primer commit

---
+++ Raices/src/Raices_App.java
@@ -0,0 +1,13 @@
+
+public class Raices_App {
+ 
+	    public static void main(String[] args) {
+	    	double a=3;
+	    	double b=9;
+	    	double c=8;
+	       Raices e=new Raices(a,b,c);
+	       e.calcular(); 
+	        
+	    }
+	     
+	}
